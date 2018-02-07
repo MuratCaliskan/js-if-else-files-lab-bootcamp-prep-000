cf9c39be31abe1d84170a8f69c65d6d44e784a6b
Murat Caliskan <muratcnjid@hotmail.com>
2018-02-07 05:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var name = "Susan"
-var height = "70"
+var name = "Susan";
+var height = "70";
 
 // Don't worry about this
 module.exports = { name, height
-}
+};
 
 if (name === "Susan") {
-    console.log("The name is correct")
+    console.log("The name is correct");
 } else {
-    console.log("Expected the name to be Susan")
+    console.log("Expected the name to be Susan");
 }
